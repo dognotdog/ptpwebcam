ef82d7eebf00914d96770c13dfe9f0343a5470ba
dognotdog <dognotdog@gmail.com>
2020-07-27 15:42:56 -0400

    add launchd service skeleton

---
+++ PtpWebcamAssistant/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  PtpWebcamAssistant
+//
+//  Created by Dömötör Gulyás on 27.07.2020.
+//  Copyright © 2020 InRobCo. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+
+@end
---
+++ PtpWebcamAssistant/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PtpWebcamAssistant
+//
+//  Created by Dömötör Gulyás on 27.07.2020.
+//  Copyright © 2020 InRobCo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // Setup code that might create autoreleased objects goes here.
+	}
+	return NSApplicationMain(argc, argv);
+}
