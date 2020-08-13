837257d914e2bc6c682a7d4752d16f3f1a710ad6
dognotdog <dognotdog@gmail.com>
2020-08-13 13:03:01 -0400

    add convenience functions for NSArray/NSDictionary to better support dealing with immutable containers

---
+++ PtpWebcamDalPlugin/FoundationExtensions.h
@@ -0,0 +1,97 @@
+//
+//  FoundationExtensions.h
+//  common
+//
+//  Created by Dömötör Gulyás on 18.01.12.
+//  Copyright (c) 2012-2020 Doemoetoer Gulyas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+dispatch_source_t dispatch_coalesce_source_create(dispatch_queue_t queue);
+void dispatch_coalesce(dispatch_source_t src, double time, void (^block)(void));
+
+
+@interface NSArray (FoundationExtensions)
+
+- (NSArray*) map: (id (^)(id obj)) block;
+- (NSArray*) indexedMap: (id (^)(id obj, NSInteger index)) block;
+- (NSArray*) select: (BOOL (^)(id obj)) block;
+
+- (NSArray*) arrayByRemovingObjectsAtIndexes: (NSIndexSet*) indexes;
+- (NSArray*) arrayByInsertingObjects: (NSArray*) ary atIndexes: (NSIndexSet*) indexes;
+- (NSArray*) arrayByInsertingObject: (id) obj atIndex: (NSUInteger) index;
+
+- (NSArray*) arrayByRemovingObject: (id) obj;
+- (NSArray*) arrayByRemovingObjectsInArray: (NSArray*) ary;
+- (NSArray*) arrayByRemovingLastObject;
+
+//- (NSArray*) continuousSubarraysWithCommonProperty: (BOOL (^)(id referenceObject, id obj)) block;
+- (NSArray*) arraysByDeinterleavingColumns: (NSUInteger) numColumns;
+- (NSArray*) arraysBySlicingAfterLimit: (NSUInteger) numLimit;
+
+@end
+
+static inline void* memcpy_ntohs(uint16_t* dst, const void* src, size_t count)
+{
+	memcpy(dst, src, 2*count);
+	for (int i = 0; i < count; ++i)
+		dst[i] = ntohs(dst[i]);
+	
+	return dst;
+}
+
+static inline void* memcpy_ntohl(uint32_t* dst, const void* src, size_t count)
+{
+	memcpy(dst, src, 4*count);
+	for (int i = 0; i < count; ++i)
+		dst[i] = ntohl(dst[i]);
+	
+	return dst;
+}
+
+
+// http://burtleburtle.net/bob/hash/doobs.html
+// Bob Jenkins' one-at-a-time hash function
+static inline uint32_t one_at_a_time_hash32(const uint8_t *key, size_t len)
+{
+	uint32_t   hash = 0;
+	for (size_t i=0; i<len; ++i)
+	{
+		hash += key[i];
+		hash += (hash << 10);
+		hash ^= (hash >> 6);
+	}
+	hash += (hash << 3);
+	hash ^= (hash >> 11);
+	hash += (hash << 15);
+	return hash;
+}
+
+@interface NSSet (FoundationExtensions)
+
+- (NSSet*) xorSetWithSet: (NSSet*) set;
+
+@end
+
+@interface NSDictionary (FoundationExtensions)
+
+- (NSDictionary*) dictionaryBySettingObject: (id) obj forKey: (id<NSCopying>) key;
+
+@end
+
+#define SuppressPerformSelectorLeakWarning(Stuff) \
+do { \
+_Pragma("clang diagnostic push") \
+_Pragma("clang diagnostic ignored \"-Warc-performSelector-leaks\"") \
+Stuff; \
+_Pragma("clang diagnostic pop") \
+} while (0)
+
+#define SuppressSelfCaptureWarning(Stuff) \
+do { \
+_Pragma("clang diagnostic push") \
+_Pragma("clang diagnostic ignored \"-Warc-retain-cycles\"") \
+Stuff; \
+_Pragma("clang diagnostic pop") \
+} while (0)
---
+++ PtpWebcamDalPlugin/FoundationExtensions.m
@@ -0,0 +1,168 @@
+//
+//  FoundationExtensions.m
+//  common
+//
+//  Created by Dömötör Gulyás on 18.01.12.
+//  Copyright (c) 2012-2020 Doemoetoer Gulyas. All rights reserved.
+//
+
+#import "FoundationExtensions.h"
+
+dispatch_source_t dispatch_coalesce_source_create(dispatch_queue_t queue)
+{
+    dispatch_source_t src = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+	dispatch_resume(src);
+	return src;
+}
+
+void dispatch_coalesce(dispatch_source_t src, double time, void (^block)(void))
+{
+	dispatch_source_set_timer(src, dispatch_time(DISPATCH_TIME_NOW, time * NSEC_PER_SEC), DISPATCH_TIME_FOREVER, 0);
+	dispatch_source_set_event_handler(src, block);
+}
+
+
+@implementation NSArray (FoundationExtensions)
+
+- (NSArray *)map: (id (^)(id obj))block
+{
+	NSMutableArray *new = [[NSMutableArray alloc] initWithCapacity: [self count]];
+	for(id obj in self)
+	{
+		@autoreleasepool {
+			id newObj = block(obj);
+			[new addObject: newObj ? newObj : [NSNull null]];
+		}
+	}
+	return new;
+}
+
+- (NSArray *) indexedMap: (id (^)(id obj, NSInteger index))block
+{
+	NSMutableArray *new = [[NSMutableArray alloc] initWithCapacity: [self count]];
+	NSInteger i = 0;
+	for(id obj in self)
+	{
+		id newObj = block(obj, i);
+		[new addObject: newObj ? newObj : [NSNull null]];
+		++i;
+	}
+	return new;
+}
+
+- (NSArray*) select: (BOOL (^)(id obj)) block
+{
+	NSMutableArray *new = [[NSMutableArray alloc] initWithCapacity: [self count]];
+	for(id obj in self)
+	{
+		BOOL keep = block(obj);
+		if (keep)
+			[new addObject: obj];
+	}
+	return new;
+
+}
+
+- (NSArray*) arrayByRemovingObjectsAtIndexes: (NSIndexSet*) indexes
+{
+	NSMutableArray* ary = [self mutableCopy];
+	[ary removeObjectsAtIndexes: indexes];
+	return ary;
+}
+
+- (NSArray*) arrayByInsertingObjects: (NSArray*) objects atIndexes: (NSIndexSet*) indexes
+{
+	NSMutableArray* ary = [self mutableCopy];
+	
+	[ary insertObjects: objects atIndexes: indexes];
+	
+	return ary;
+}
+- (NSArray*) arrayByInsertingObject:(id)obj atIndex:(NSUInteger)index
+{
+	NSMutableArray* ary = [self mutableCopy];
+	[ary insertObject: obj  atIndex: index];
+	return ary;
+}
+
+- (NSArray*) arrayByRemovingObject:(id)obj
+{
+	NSMutableArray* ary = [self mutableCopy];
+	[ary removeObject: obj];
+	return ary;
+}
+
+- (NSArray*) arrayByRemovingObjectsInArray: (NSArray*) ary
+{
+	NSMutableArray* result = [self mutableCopy];
+	[result removeObjectsInArray: ary];
+	return result;
+}
+- (NSArray*) arrayByRemovingLastObject
+{
+	NSMutableArray* result = [self mutableCopy];
+	[result removeLastObject];
+	return result;
+}
+
+- (NSArray*) arraysBySlicingAfterLimit: (NSUInteger) numLimit
+{
+	NSMutableArray* array = [NSMutableArray arrayWithCapacity: self.count/numLimit+1];
+	NSUInteger numSlices = ((self.count+numLimit-1)/numLimit);
+	for (NSUInteger i = 0; i < numSlices; ++i)
+		[array addObject: [[NSMutableArray alloc] init]];
+
+	[self enumerateObjectsUsingBlock: ^(id obj, NSUInteger idx, BOOL *stop) {
+		[[array objectAtIndex: idx/numLimit] addObject: obj];
+	}];
+
+	
+	return array;
+}
+
+- (NSArray*) arraysByDeinterleavingColumns: (NSUInteger) numColumns
+{
+	NSMutableArray* columns = [NSMutableArray arrayWithCapacity: numColumns];
+	for (NSUInteger i = 0; i < numColumns; ++i)
+		[columns addObject: [[NSMutableArray alloc] init]];
+	
+	[self enumerateObjectsUsingBlock: ^(id obj, NSUInteger idx, BOOL *stop) {
+		[[columns objectAtIndex: (idx % numColumns)] addObject: obj];
+	}];
+	
+	return columns;
+}
+
+@end
+
+
+@implementation NSSet (FoundationExtensions)
+
+- (NSSet*) xorSetWithSet: (NSSet*) set
+{
+	NSMutableSet* a = [self mutableCopy];
+	NSMutableSet* b = [set mutableCopy];
+	[a minusSet: set];
+	[b minusSet: self];
+	[a unionSet: b];
+	return a;
+}
+
+@end
+
+@implementation NSDictionary (FoundationExtensions)
+
+- (NSDictionary*) dictionaryBySettingObject: (id) obj forKey: (id<NSCopying>) key
+{
+	NSMutableDictionary* dict = [self mutableCopy];
+	[dict setObject: obj forKey: key];
+	return dict;
+}
+
+@end
+
+
+
+
+
+
