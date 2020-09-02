6760f6539781030b568a2dbc103d2fe1e6c32584
dognotdog <dognotdog@gmail.com>
2020-09-01 21:07:46 -0400

    first pong received from simple assistant

--- PtpWebcamSimpleAssistant/main.m
+++ PtpWebcamSimpleAssistant/main.m
@@ -6,57 +6,87 @@
 //  Copyright © 2020 Doemoetoer Gulyas. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "../PtpWebcamDalPlugin/PtpWebcamAlerts.h"
+#import "../PtpWebcamDalPlugin/FoundationExtensions.h"
+#import "../PtpWebcamAssistantService/PtpWebcamAssistantServiceProtocol.h"
 
+
+#import <Foundation/Foundation.h>
 #include <servers/bootstrap.h>
 
-boolean_t MessagesAndNotifications(mach_msg_header_t* request, mach_msg_header_t* reply)
+@interface PtpWebcamAssistantListener : NSObject <NSXPCListenerDelegate>
+
+@property NSArray* connections;
+
+@end
+
+@implementation PtpWebcamAssistantListener
+
+- (void) startListening
 {
-	return true;
+	NSXPCListener* listener = [[NSXPCListener alloc] initWithMachServiceName: @"org.ptpwebcam.PtpWebcamAssistant"];
+	listener.delegate = self;
+	[listener resume];
+
 }
+
+- (BOOL)listener:(NSXPCListener *)listener shouldAcceptNewConnection:(NSXPCConnection *)newConnection
+{
+    // This method is where the NSXPCListener configures, accepts, and resumes a new incoming NSXPCConnection.
+	    
+    // Configure the connection.
+    // First, set the interface that the exported object implements.
+    newConnection.exportedInterface = [NSXPCInterface interfaceWithProtocol:@protocol(PtpWebcamAssistantServiceProtocol)];
+	
+//	NSXPCInterface* cameraInterface = [NSXPCInterface interfaceWithProtocol:@protocol(PtpCameraProtocol)];
+	
+	NSXPCInterface* remoteInterface = [NSXPCInterface interfaceWithProtocol:@protocol(PtpWebcamAssistantDelegateProtocol)];
+//	[remoteInterface setInterface: cameraInterface forSelector: @selector(cameraConnected:) argumentIndex: 0 ofReply: NO];
+	
+	
+	newConnection.remoteObjectInterface = remoteInterface;
+    
+    // Next, set the object that the connection exports. All messages sent on the connection to this service will be sent to the exported object to handle. The connection retains the exported object.
+    newConnection.exportedObject = self;
+    
+	newConnection.invalidationHandler = ^{
+		PtpLog(@"connection died");
+		@synchronized (self) {
+			self.connections = [self.connections arrayByRemovingObject: newConnection];
+		}
+	};
+
+	@synchronized (self) {
+		self.connections = [self.connections arrayByAddingObject: newConnection];
+	}
+
+    // Resuming the connection allows the system to deliver more incoming messages.
+    [newConnection resume];
+	    
+    // Returning YES from this method tells the system that you have accepted this connection. If you want to reject the connection for some reason, call -invalidate on the connection and return NO.
+    return YES;
+}
+
+- (void) pingService: (void (^)(void)) pongCallback;
+{
+	pongCallback();
+}
+
+@end
+
+
 
 int main(int argc, const char * argv[]) {
 	@autoreleasepool {
-	    // insert code here...
 	    NSLog(@"Hello, World!");
 
-		// Check in with the bootstrap port under the agreed upon name to get the servicePort with receive rights
-		mach_port_t servicePort;
-		name_t serviceName = "org.ptpwebcam.PtpWebcamAssistant";
-		kern_return_t err = bootstrap_check_in(bootstrap_port, serviceName, &servicePort);
-		if (BOOTSTRAP_SUCCESS != err)
-		{
-			NSLog(@"bootstrap_check_in() failed: 0x%x", err);
-			exit(43);
-		}
-	
-		#if 0
-			// Wait forever until the Debugger can attach to the Assistant process
-			bool waiting = true;
-			while (waiting)
-			{
-				sleep(1);
-			}
-		#endif
-
-		// Add the service port to the Assistant's port set
-		mach_port_t portSet = MACH_PORT_NULL;
-		// Create a port set to hold the service port, and each client's port
-		err = mach_port_allocate(mach_task_self(), MACH_PORT_RIGHT_PORT_SET, &portSet);
-
-		err = mach_port_move_member(mach_task_self(), servicePort, portSet);
-		if (KERN_SUCCESS != err)
-		{
-			NSLog(@"Unable to add service port to port set: 0x%x", err);
-			exit(2);
-		}
-
-		// Service incoming messages from the clients and notifications which were signed up for
-		NSLog(@"Entering Service Loop!");
-		while (true)
-		{
-			(void) mach_msg_server(MessagesAndNotifications, 8192, portSet, MACH_MSG_OPTION_NONE);
-		}
+		
+		PtpWebcamAssistantListener* listener = [[PtpWebcamAssistantListener alloc] init];
+		
+		[listener startListening];
+		
+		[[NSRunLoop currentRunLoop] run];
+		
 		NSLog(@"Bye, World!");
 	}
 	return 0;
