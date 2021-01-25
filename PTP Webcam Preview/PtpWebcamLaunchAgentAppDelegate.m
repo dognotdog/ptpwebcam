5636abba7bff67eb5aaaef7abc2fe5f4ca17245d
dognotdog <dognotdog@gmail.com>
2021-01-25 15:35:36 -0500

    added object introspection functions for debugging to preview app

--- PTP Webcam Preview/PtpWebcamLaunchAgentAppDelegate.m
+++ PTP Webcam Preview/PtpWebcamLaunchAgentAppDelegate.m
@@ -8,11 +8,40 @@
 
 #import "PtpWebcamLaunchAgentAppDelegate.h"
 
+#import <objc/runtime.h>
+
+
+int _printIvars(id obj)
+{
+	NSUInteger count = 0;
+	Ivar *vars = class_copyIvarList([obj class], &count);
+	for (NSUInteger i = 0; i < count; i++) {
+		Ivar var = vars[i];
+		NSLog(@"%s %s", ivar_getName(var), ivar_getTypeEncoding(var));
+	}
+	free(vars);
+	return 0;
+}
+
+int _printMethods(Class class)
+{
+	NSLog(@"%s", class_getName(class));
+	unsigned int count = 0;
+	Method *methods = class_copyMethodList(class, &count);
+	for (size_t i = 0; i < count; i++) {
+		Method method = methods[i];
+		NSLog(@"  %s %s", sel_getName(method_getName(method)), method_getTypeEncoding(method));
+	}
+
+	return 0;
+}
+
 @interface PtpWebcamLaunchAgentAppDelegate ()
 {
 }
 
 @property (weak) IBOutlet NSWindow *window;
+
 @end
 
 static PtpWebcamLaunchAgentAppDelegate* _sharedAppDelegate = nil;
@@ -26,10 +55,7 @@ static PtpWebcamLaunchAgentAppDelegate* _sharedAppDelegate = nil;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
 	_sharedAppDelegate = self;
-	
-	
-//	NSLog(@"devices %@", deviceBrowser.devices);
-	
+
 }
 
 
